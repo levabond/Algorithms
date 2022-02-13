cf6f3e1a90a509e8ac7638f5f2f7d8458c1c960c
Lev Bondarenko <lion.bond93@gmail.com>
2022-02-13 22:03:59 +0500

    Binary Search
    - Guess Number

---
+++ Algorithms/Algorithms/LeetCode/BinarySearch/GuessNumber.swift
@@ -0,0 +1,33 @@
+//
+//  GuessNumber.swift
+//  Algorithms
+//
+//  Created by Лев Бондаренко on 13.02.2022.
+// https://leetcode.com/explore/learn/card/binary-search/125/template-i/951/
+
+import Foundation
+
+class GuessNumber {
+//    func guessNumber(_ n: Int) -> Int {
+//        if guess(n) == 0 {
+//            return n
+//        }
+//        var start = 1
+//        var end = n
+//
+//        while start <= end {
+//           var mid = (start + end) / 2
+//            var resultGuess = guess(mid)
+//
+//            if resultGuess == 0 {
+//                return mid
+//            } else if resultGuess == -1 {
+//                end = mid - 1
+//            } else if resultGuess == 1 {
+//                start = mid + 1
+//            }
+//        }
+//
+//        return -1
+//    }
+}
--- Algorithms/README.md
+++ Algorithms/README.md
@@ -2,11 +2,8 @@
 
 LeetCode Online Judge is a website containing many algorithm questions. Most of them are real interview questions of Google, Facebook, LinkedIn, Apple, etc. This repo shows my solutions by Swift with the code style strictly follows the RayWenderlich Swift Style Guide. Please feel free to reference and STAR to support this repo, thank you!
 
-
 # Progress
-
 in my plans to solve 1000 tasks to escape from russia
-
 
 # Data Structures
 - Heap
@@ -34,7 +31,6 @@ in my plans to solve 1000 tasks to escape from russia
 - Find Disappeared Numbers
 - Square Of A Sorted Array
 - Hieght Checher
-
 
 
 # Numbers
