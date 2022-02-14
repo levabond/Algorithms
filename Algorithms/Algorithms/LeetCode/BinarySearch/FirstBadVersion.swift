ff623a0dbd90e6071d4f6b107e3a2873b21de80e
Lev Bondarenko <lion.bond93@gmail.com>
2022-02-14 09:48:17 +0500

    FirstBadVersion

---
+++ Algorithms/Algorithms/LeetCode/BinarySearch/FirstBadVersion.swift
@@ -0,0 +1,27 @@
+//
+//  FirstBadVersion.swift
+//  Algorithms
+//
+//  Created by Лев Бондаренко on 14.02.2022.
+//
+
+import Foundation
+
+//class FirsetBadVersion : VersionControl {
+//    func firstBadVersion(_ n: Int) -> Int {
+//        var left = 1
+//        var right = n
+//        
+//        while left < right {
+//            var mid = left + (right - left) / 2
+//            
+//            if !isBadVersion(mid) {
+//                left = mid + 1
+//            } else {
+//                right = mid
+//            }
+//        }
+//        
+//        return left
+//    }
+//}
